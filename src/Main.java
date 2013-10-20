c2d52dfca0afe964c7f4acca789b4da1b753dcbd
tuliak <grc.tula@gmail.com>
2013-10-20 16:26:31 +0400

    fist commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import com.lesson.Human;
+
+public class Main {
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.setName("Andrew");
+        human.setName("Max");
+        human.setName("Lex");
+        human.setName("Miranda");
+        System.out.println(human.getName());
+        human.setAge(-12);
+        System.out.println(human.getAge());
+        System.out.println(human.getAge());
+        human.setName(null);
+        System.out.println(human.getName());
+        human.setName("A");
+        System.out.println(human.getName());
+    }
+}
